372533fef3ff10c9189535a5f84e440331f92d37
ziyanjupan <1258811819@qq.com>
2019-10-27 16:12:09 +0800

    swagger

--- Base/src/main/java/com/fenix/base/controller/UploadController.java
+++ Base/src/main/java/com/fenix/base/controller/UploadController.java
@@ -1,7 +1,15 @@
 package com.fenix.base.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
 
 /**
  * @Author: xiaguangyong
@@ -14,6 +22,8 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController(value = "/upload")
 public class UploadController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+
     /**
      * @Description 上传文件
      * @Author xiaguangyong
@@ -21,7 +31,21 @@ public class UploadController {
      * @Param file
      * @Return java.lang.String
      */
+    @RequestMapping(value = "/file",method = RequestMethod.POST)
     public String file(MultipartFile file) {
-        return "success";
+        if (file.isEmpty()) {
+            return "请选择文件";
+        }
+        String fileName = file.getOriginalFilename();
+        String filePath = "C:/upload/";
+        File dest = new File(filePath, fileName);
+        try {
+            file.transferTo(dest);
+            logger.info("上传成功");
+            return "success";
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+            return "failed";
+        }
     }
 }
